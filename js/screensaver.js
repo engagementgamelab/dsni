e8dd95ffbefd485b8d9dbc90ca70e7a91db4fe48
russell goldenberg <russellgoldenberg@gmail.com>
2013-10-31 13:28:53 -0400

    new style

--- js/screensaver.js
+++ js/screensaver.js
@@ -3,6 +3,7 @@
 (function() {
 	var updateInterval = 65000;
 	function _update() {
+		$('.minutes').removeClass('prox1, prox2, prox3, prox4, prox5');
 		$mbta.getPredictions(15, function(err,results) {
 			if(err) {
 				console.log(err);
@@ -12,11 +13,34 @@
 					nextLangdon = sortedLangdon[0],
 					nextDennis = sortedDennis[0];
 
-				var textL = nextLangdon.title + ' arriving in ' + nextLangdon.minutes + ' minutes',
-					textD = nextDennis.title + ' arriving in ' + nextDennis.minutes + ' minutes';
+				$('.stop1485 .bus15 .minutes span').text(nextLangdon.minutes);
+				$('.stop1498 .bus15 .minutes span').text(nextDennis.minutes);
+				$('.stop1485 .bus15 .text').text(nextLangdon.title);
+				$('.stop1498 .bus15 .text').text(nextDennis.title);
 
-				$('.stop1485 .bus15 .text').text(textL);
-				$('.stop1498 .bus15 .text').text(textD);
+				//add color
+				if(nextLangdon.minutes < 3) {
+					$('.stop1485 .bus15 .minutes').addClass('prox1');
+				} else if(nextLangdon.minutes < 7) {
+					$('.stop1485 .bus15 .minutes').addClass('prox2');
+				} else if(nextLangdon.minutes < 11) {
+					$('.stop1485 .bus15 .minutes').addClass('prox3');
+				} else if(nextLangdon.minutes < 15) {
+					$('.stop1485 .bus15 .minutes').addClass('prox4');
+				} else {
+					$('.stop1485 .bus15 .minutes').addClass('prox5');
+				}
+				if(nextDennis.minutes < 3) {
+					$('.stop1498 .bus15 .minutes').addClass('prox1');
+				} else if(nextDennis.minutes < 7) {
+					$('.stop1498 .bus15 .minutes').addClass('prox2');
+				} else if(nextDennis.minutes < 11) {
+					$('.stop1498 .bus15 .minutes').addClass('prox3');
+				} else if(nextDennis.minutes < 15) {
+					$('.stop1498 .bus15 .minutes').addClass('prox4');
+				} else {
+					$('.stop1498 .bus15 .minutes').addClass('prox5');
+				}
 			}
 		});
 		$mbta.getPredictions(41, function(err, results) {
@@ -28,11 +52,33 @@
 					nextLangdon = sortedLangdon[0],
 					nextDennis = sortedDennis[0];
 
-				var textL = nextLangdon.title + ' arriving in ' + nextLangdon.minutes + ' minutes',
-				textD = nextDennis.title + ' arriving in ' + nextDennis.minutes + ' minutes';
+				$('.stop1485 .bus41 .minutes span').text(nextLangdon.minutes);
+				$('.stop1498 .bus41 .minutes span').text(nextDennis.minutes);
+				$('.stop1485 .bus41 .text').text(nextLangdon.title);
+				$('.stop1498 .bus41 .text').text(nextDennis.title);
 
-				$('.stop1485 .bus41 .text').text(textL);
-				$('.stop1498 .bus41 .text').text(textD);
+				if(nextLangdon.minutes < 3) {
+					$('.stop1485 .bus41 .minutes').addClass('prox1');
+				} else if(nextLangdon.minutes < 7) {
+					$('.stop1485 .bus41 .minutes').addClass('prox2');
+				} else if(nextLangdon.minutes < 11) {
+					$('.stop1485 .bus41 .minutes').addClass('prox3');
+				} else if(nextLangdon.minutes < 15) {
+					$('.stop1485 .bus41 .minutes').addClass('prox4');
+				} else {
+					$('.stop1485 .bus41 .minutes').addClass('prox5');
+				}
+				if(nextDennis.minutes < 3) {
+					$('.stop1498 .bus41 .minutes').addClass('prox1');
+				} else if(nextDennis.minutes < 7) {
+					$('.stop1498 .bus41 .minutes').addClass('prox2');
+				} else if(nextDennis.minutes < 11) {
+					$('.stop1498 .bus41 .minutes').addClass('prox3');
+				} else if(nextDennis.minutes < 15) {
+					$('.stop1498 .bus41 .minutes').addClass('prox4');
+				} else {
+					$('.stop1498 .bus41 .minutes').addClass('prox5');
+				}
 			}
 		});
 		setTimeout(_update, updateInterval);
